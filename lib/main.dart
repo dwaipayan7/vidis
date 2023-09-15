d7f50a380583785fdecc415fd7f43950571bfc31
Dwaipayan Biswas <biswasdwai007@gmail.com>
2023-09-16 04:16:09 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:vidis/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DisiV',
+      theme: ThemeData.dark().copyWith(
+
+       //  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       // scaffoldBackgroundColor: Colors.blue,
+      ),
+      routes: {
+        '/login': (context) => LoginScreen(),
+      },
+      home: LoginScreen(),
+    );
+  }
+}
